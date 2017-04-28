c9550a76572399a0c634f04928f91e1b3b664c1e
kurczewski7 <slawomir.kurczewski@gmail.com>
2017-04-28 22:56:24 +0200

    dodano price do ulubionych

---
+++ DVDShop/FavoriteCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteCollectionViewCell.swift
+//  DVDShop
+//
+//  Created by Slawek Kurczewski on 28.04.2017.
+//  Copyright © 2017 Slawomir Kurczewski. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCollectionViewCell: UICollectionViewCell {
+    
+}
